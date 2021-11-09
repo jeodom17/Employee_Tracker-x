1ac9f547678e2ec70c07d81615d11bd8c3574956
jeodom17 <jeodom17@gmail.com>
2021-11-09 17:23:00 -0500

    psuedo coding

--- index.js
+++ index.js
@@ -4,23 +4,86 @@ const { inherits } = require('util');
 const db = require("./db");
 const chalk = require('chalk');
 const logo = require("asciiart-logo");
+require("console.table");
 
 
 init();
 
-//* Display logo Text, load main prompts
+//* Display logo Text, initialize first question
 function init() {
     const logoText = logo({ 
-        name: "Company X Employee Manager",
+        name: "Employee Manager",
         textColor: 'blue',
         logoColor: 'bold-green',
-        borderColor: 'bold-red',
+        borderColor: 'blue',
         font: 'Georgia11',
     })
     .render();
   
     console.log(logoText);
  
-
+    handleInitialQuestion();
 
 } 
+
+//* function for ^ handleInitialQuestion -- prompt
+
+
+function handleInitialQuestion() {
+    prompt([
+        {
+            type: "list",
+            name: "userOptions",
+            message: "Please choose from the following options to get started."
+            choices: [
+                {
+                    name:
+                    value:
+                },
+            ]
+        }
+
+
+    ]).then((answers) => {
+        // Use user feedback for... whatever!!
+      })
+      .catch((error) => {
+        if (error.isTtyError) {
+          // Prompt couldn't be rendered in the current environment
+        } else {
+          // Something else went wrong
+        }
+      });
+}
+
+
+
+
+//* function called per user choice -- use switch case for each user answer option --> case "choices value (in name value pair)": --> call function(); --> break; --> next user option
+
+
+
+
+
+
+
+
+
+
+
+
+//^ Functions for all user choice options
+
+
+
+
+
+
+
+
+
+
+
+
+
+
