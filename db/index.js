e3b0908eb020ba92ef33349437501d0cf0dbd13e
jeodom17 <jeodom17@gmail.com>
2021-11-09 21:11:54 -0500

    add comments

--- db/index.js
+++ db/index.js
@@ -4,17 +4,36 @@ class DB {
     constructor(connection) {
       this.connection = connection;
     }
-
+//* find all departments
     handleFindDepartments() {
         return this.connection.promise().query(
           "SELECT department.id, department.name FROM department;"
         );
       }
-
+//* find all roles
       handleFindRoles() {
         return this.connection.promise().query(
           "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
         );
       }
+//* find all employees
+
+
+//* add a new department
+
+
+
+//* add a new role
+
+
+
+//* add a new employee
+
+
+
+
+//* update employee role
+
+
 
 }
