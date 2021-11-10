71aaf92d003885108dbbe9b64a62820e58db4b66
jeodom17 <jeodom17@gmail.com>
2021-11-09 20:11:57 -0500

    set up connection.js , .env

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.DB_PASS,
+  database: "employees"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+});
+
+module.exports = connection;
